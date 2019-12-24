47e45b1eb7e0094b3761cec3f5a4fd0b80d23894
Konrad <dexus1992@gmail.com>
2019-12-24 13:56:44 +0100

    first commit

---
+++ Assets/Map/base tileset.tsx
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../Textures/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="60"/>
+ </terraintypes>
+ <tile id="2" terrain=",,,0">
+  <objectgroup draworder="index">
+   <object id="4" x="8.90909" y="1.45455" width="7.27273" height="6.72727"/>
+   <object id="5" x="5.45455" y="3.63636" width="7.81818" height="3.81818"/>
+   <object id="6" x="4" y="5.63636" width="6.72727" height="1.63636"/>
+   <object id="7" x="2.72727" y="7.81818" width="9.09091" height="2.72727"/>
+   <object id="8" x="2" y="9.09091" width="8" height="6.90909"/>
+   <object id="9" x="7.09091" y="6.18182" width="9.09091" height="9.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="2.36364" width="15.4545" height="13.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain=",,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="11.6364"/>
+   <object id="2" x="-4" y="12.1818"/>
+   <object id="3" x="-3.09091" y="12.1818" width="0.545455" height="0.181818"/>
+   <object id="4" x="-0.909091" y="12.9091" width="0.545455" height="0.181818"/>
+   <object id="5" x="2.18182" y="14.3636"/>
+   <object id="6" x="2.54545" y="14.7273" width="0.363636" height="0.909091"/>
+   <object id="7" x="4" y="17.4545" width="0.363636" height="0.727273"/>
+   <object id="8" x="5.81818" y="19.6364" width="0.181818" height="0.363636"/>
+   <object id="9" x="3.45455" y="19.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="57" terrain="0,0,0,"/>
+ <tile id="58" terrain="0,0,,0"/>
+ <tile id="59" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="12.3636" y="9.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="60" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.238636" y="-0.068182" width="15.6364" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="3.09091" y="9.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="114" terrain="0,,0,0"/>
+ <tile id="115" terrain=",0,0,0"/>
+ <tile id="116" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="2" x="12.7273" y="-5.81818"/>
+   <object id="3" x="13.6364" y="-2.54545"/>
+   <object id="4" x="14.3636" y="-0.363636"/>
+   <object id="5" x="16" y="0.727273"/>
+   <object id="6" x="16.7273" y="1.63636"/>
+   <object id="7" x="18.9091" y="2.72727"/>
+   <object id="8" x="21.6364" y="3.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="117" terrain="0,0,,">
+  <objectgroup draworder="index">
+   <object id="1" x="5.63636" y="2.90909"/>
+  </objectgroup>
+ </tile>
+ <tile id="118" terrain="0,,,">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="-6.36364" height="1.09091"/>
+   <object id="2" x="1.81818" y="0"/>
+   <object id="3" x="0" y="2.90909"/>
+   <object id="4" x="-2" y="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="528">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="15.0909" height="15.8182"/>
+  </objectgroup>
+ </tile>
+</tileset>
